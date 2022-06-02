8466ec6c37eb66a2c0bf422e71b794c7467ff60e
xkcm <xkcm16@gmail.com>
2022-06-02 15:11:02 +0200

    Remove Project parameter

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const kaholo = require("@kaholo/plugin-library");
 const gcpCli = require("./gcpcli");
 
 async function runCommand(parameters) {
-  let additionalFlags = "--format=json ";
-  if (parameters.project) {
-    additionalFlags += `--project=${parameters.project} `;
-  }
+  const additionalFlags = "--format=json";
 
   try {
     const result = await gcpCli.execute({
@@ -23,13 +20,10 @@ async function runCommand(parameters) {
 }
 
 async function runGsutilCommand(parameters) {
-  const additionalFlags = parameters.project ? `-p ${parameters.project}` : "";
-
   try {
     const result = await gcpCli.execute({
       credentials: parameters.credentials,
       command: parameters.command,
-      additionalFlags,
       workingDirectory: parameters.workingDirectory,
       cliTool: "gsutil",
     });
